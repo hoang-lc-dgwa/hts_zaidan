582490ffcde5620f4ed8a746412862463df29e5e
hoang-lc-dgwa <hoang-lc@dgw.asia>
2024-10-08 16:09:49 +0700

    Add theme wp public to diff

---
+++ Diff/diff_work_local_source_01_no_file_bk_20241007/HTML_noFileBackup/cms/wp-content/themes/twentytwelve_child_kif_190815/scholarship.php
@@ -0,0 +1,273 @@
+<?php
+/*
+Template Name: 奨学金
+*/
+?>
+
+<?php get_header() ?>
+
+<body>
+<img src="/img/back_1.jpg" alt="" class="bgmaximage" />
+<div id="back1">
+
+<!--header-->
+<div id="header">
+<div id="header_line"></div>
+<div id="header_white">
+<div id="in_header">
+<div id="logo" class="left"><a href="/"><img src="/img/logo.png" alt="" width="329" height="55"></a></div>
+<div class="right_head"><img src="/img/masseage.png" alt="" width="341" height="42">
+<p><img src="/img/a_outline.png" alt="" width="60" height="60"></p>
+</div>
+</div>
+</div>
+</div>
+<!--end header-->
+<div style="clear:both;"></div>
+
+<div id="wrapper">
+<div id="container">
+<div id="main_container">
+
+
+<!--左ナビ-->
+
+<div id="left_body">
+<div id="side_line_down_requ"></div>
+<div id="side_block">
+
+<div class="navi_block">
+<div id="home_btn">
+<div id="side_totop"><a href="/"><img src="/common/img/navi/side_top_rollout.png" alt="トップページへ" width="220" height="30" border="0"></a></div>
+</div>
+</div>
+
+<div id="sidenavi">
+
+<!--財団概要ブロック-->
+<script type="text/javascript" src="/common/js/overview.js"></script>
+<script type="text/javascript">
+	overview();
+</script>
+<!--財団概要ブロックここまで-->
+
+
+<!--activityブロック-->
+<div class="navi_block">
+<div id="navi_b_head"><span>活動内容</span></div>
+
+
+<!--活動内容-->
+<ul class="sf-menu sf-vertical">
+
+
+
+<!--年間スケジュール-->
+<li><a href="/activity/schedule.html"><img src="/common/img/nav_img/nav_activity_sche_rollout.png" alt="年間スケジュール" width="220" height="30" border="0"></a>
+</li>
+<!--年間スケジュール-->
+
+
+
+<!--奨学金支給事業-->
+<li><a href="/scholarship/"><img src="/common/img/nav_img/nav_activity_scl_rollover.png" alt="奨学金支給事業" width="220" height="30" border="0"></a></li>
+
+
+<li><a href="/activity/scholarship_zaidan.html"><img src="/common/img/nav_img/nav_activity_scl_zaidan_rollout.png" alt="一般財団法人 共立国際交流奨学財団奨学金" width="220" height="30" border="0"></a></li>
+
+<li><a href="/activity/scholarship_maintenance.html"><img src="/common/img/nav_img/nav_activity_scl_ment_simple_rollout.png" alt="株式会社共立メンテナンス奨学基金奨学金" width="220" height="30" border="0"></a></li>
+<li><a href="/activity/scholarship_youngsteel.html"><img src="/common/img/nav_img/nav_activity_scl_youngsteel_simple_rollout.png"  alt="ヤングスチール株式会社奨学金" width="220" height="30" border="0"></a></li>
+<li><a href="/activity/list.html"><img src="/common/img/nav_img/nav_activity_scl_list_rollout.png" alt="奨学金対象校リスト" width="220" height="30" border="0"></a></li>
+
+<li><a href="/information/localscholarship.html" ><img src="/common/img/nav_img/nav_activity_scl_local_rollout.png" alt="現地支給奨学金（ベトナム・ミャンマー・ラオス）" width="220" height="39" border="0"></a></li>
+
+<!--奨学金支給事業-->
+
+
+<!--生活・学習情報提供事業-->
+<li><img src="/common/img/nav_img/nav_life_rollout.png" alt="生活・学習情報提供事業" width="220" height="30" border="0">
+<ul>
+<li><a href="/activity/publication.html"><img src="/common/img/nav_img/nav_life_pub_rollout.png" alt="出版物の紹介" width="220" height="30" border="0"></a></li>
+</ul>
+</li>
+<!--生活・学習情報提供事業-->
+
+
+
+<!--国際交流支援事業-->
+<li><img src="/common/img/nav_img/nav_in_rollout.png" alt="国際交流支援事業" width="220" height="30" border="0">
+
+<ul>
+
+
+<!--ホームステイ研修会-->
+<li><a href="/homestay/"><img src="/common/img/nav_img/nav_home_rollout.png" alt="ホームステイ研修会" width="220" height="30" border="0"></a>
+
+</li><!--ホームステイ研修会-->
+
+
+
+<!--コンテスト-->
+<li><a href="/contest_slection/"><img src="/common/img/nav_img/nav_cl_rollout.png" alt="コンテスト" width="220" height="30" border="0"></a></li><!--コンテスト-->
+
+
+<!--国際ボランティア支援基金-->
+<!-- <li><a href="/activity/volunteer.html"><img src="/common/img/nav_img/nav_vol_rollout.png" alt="国際ボランティア支援基金" width="220" height="30" border="0"></a></li> -->
+<!--国際ボランティア支援基金-->
+
+
+</ul>
+</li>
+<!--国際交流支援事業-->
+
+
+
+<!--[NAVIGATION][NEW UPDATE 20190918]-->
+
+<!--寮事業-->
+<li><a href="/dormy/"><img src="/common/img/nav_img/nav_dormy_rollout.png" alt="寮事業" width="220" height="30" border="0"></a>
+</li>
+<!--寮事業-->
+
+<!--[NAVIGATION][NEW UPDATE 20190918]-->
+
+
+<!-- 就職支援事業 -->
+<li><a href="/jobsupport/"><img src="/common/img/nav_img/nav_life_recruit_rollout.png" alt="就職支援事業" width="220" height="30" border="0"></a></li>
+<!-- 就職支援事業 -->
+
+</ul>
+<!--活動内容-->
+
+<br style="clear:both">
+</div>
+<!--activityブロックここまで-->
+
+
+
+
+<!--officeブロック-->
+<script type="text/javascript" src="/common/js/office.js"></script>
+<script type="text/javascript">
+	office();
+</script>
+<!--officeブロックここまで-->
+
+
+
+
+<!--メディア-->
+<div class="navi_block">
+<ul class="normal_navi n_media">
+<li><a href="/media/"><img src="/common/img/nav_img/nav_g_media_rollout.png" alt="メディア掲載" width="220" height="30" border="0"></a></li>
+</ul>
+</div><!--メディア-->
+
+
+
+<!--recruitブロック-->
+<script type="text/javascript" src="/common/js/recruit.js"></script>
+<script type="text/javascript">
+	recruit();
+</script>
+<!--recruitブロックここまで-->
+
+
+
+
+
+
+</div><!--サイドナビここまで-->
+</div><!--side_block-->
+
+<div style="clear:both;"></div>
+
+<!--bannerブロック-->
+<script type="text/javascript" src="/common/js/banner.js"></script>
+<script type="text/javascript">
+	banner();
+</script>
+<!--bannerブロックここまで-->
+
+</div><!--left_body-->
+
+<!--左ナビ-->
+
+
+<div id="main">
+<div id="main_in">
+
+<h1><img src="/common/img/scholarship_head.png" alt="" width="610" height="200" border="0" id="imghead"></h1>
+
+
+
+<div id="info_box">
+
+<?php
+   $newslist = get_posts( array(
+    'category_name' => 'scholarship_info', //特定のカテゴリースラッグを指定
+    'posts_per_page' => 10 //取得記事件数
+  ));
+    foreach( $newslist as $post ):
+    setup_postdata( $post );
+?>
+<dl>
+<dt class="<?php echo post_custom('アイコン'); ?>"> <?php the_time('Y年n月j日'); ?></dt>
+<dd>
+
+<?php $ctm = get_post_meta($post->ID, 'リンク先', true);?>
+<?php if(empty($ctm)):?>
+<a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a>
+
+<?php else:?>
+<a href="<?php echo post_custom('リンク先'); ?>" <?php if(get_post_meta($post->ID,'PDF',true)): ?> target="_blank" class="pdflink"<?php endif; ?>><?php the_title();?></a>
+
+<?php endif;?>
+</dd>
+</dl>
+<div class="dot_line_s"></div>
+<?php
+  endforeach;
+  wp_reset_postdata();
+?>
+</div>
+
+
+
+
+<?php
+  if (have_posts()) : while (have_posts()) : the_post();
+    the_content();
+  endwhile; endif;
+?>
+
+
+<div style="clear:both;"></div>
+
+
+
+
+</div><!--main_in-->
+</div><!--main container-->
+</div><!--end container-->
+
+<div style="clear:both;"></div>
+
+
+<div id="back-top_pos_wide">
+<p id="back-top_wide"><a href="#top"><span><img src="/img/to_top.png" alt="ページトップへ戻る" width="30" height="130"></span></a></p>
+</div>
+</div>
+</div><!--end wrapper-->
+
+<!--foot-->
+<script type="text/javascript" src="/common/js/foot.js"></script>
+<script type="text/javascript">
+	foot();
+</script>
+<!--foot-->
+
+</div><!--end back1-->
+
+</body>
+</html>
