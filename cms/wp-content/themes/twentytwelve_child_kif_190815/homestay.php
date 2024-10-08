f97f87ff0292ad015125521959f20f791bcdac01
hoang-lc-dgwa <hoang-lc@dgw.asia>
2024-10-08 16:05:50 +0700

    Add theme wp public

---
+++ cms/wp-content/themes/twentytwelve_child_kif_190815/homestay.php
@@ -0,0 +1,726 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<title>ホームステイ研修会 | 一般財団法人 共立国際交流奨学財団</title>
+
+<link href="/common/css/common.css" rel="stylesheet" type="text/css">
+<link href="/common/css/main.css" rel="stylesheet" type="text/css">
+<link href="/common/css/homestay.css" rel="stylesheet" type="text/css">
+
+
+
+
+
+<link href="/common/css/navi.css" rel="stylesheet" type="text/css" media="all" />
+
+<script type="text/javascript" src="/125B8E7A9EB54671AC526CF40C3E2901/1B31C111-D9DE-B34A-854F-BEE847C32535/main.js" charset="UTF-8"></script><script type="text/javascript">
+
+$(function(){
+
+    $('img.bgmaximage').maxImage({
+
+        isBackground: true,
+
+        overflow: 'auto',
+
+        verticalAlign:'top'
+
+    });
+
+});
+
+</script>
+
+
+
+
+
+<script src="/common/js/import.js" type="text/javascript"></script>
+
+
+
+
+<script language="JavaScript" type="text/javascript">
+
+<!--
+
+function disp(url){
+
+window.open(url,"window_name","toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes");
+
+}
+
+//-->
+
+</script>
+
+<!-- トップに戻る-->
+
+
+
+<script src="../common/js/ga.js" type="text/javascript"></script>
+
+
+
+
+<script src="/common/js/homestay.js" type="text/javascript"></script>
+
+</head>
+
+
+
+<body>
+
+<a name="top"></a>
+
+<img src="/img/back_1.jpg" alt="" class="bgmaximage" />
+
+<div id="back1">
+
+<!--header-->
+
+<div id="header">
+
+<div id="header_line"></div>
+
+<div id="header_white">
+
+<div id="in_header">
+
+<div id="logo" class="left"><a href="/"><img src="/img/logo.png" alt="" width="329" height="55"></a></div>
+
+<div class="right_head"><img src="/img/masseage.png" alt="" width="341" height="42">
+
+<p><img src="/img/o_outline.png" alt="" width="60" height="60"></p>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<!--end header-->
+
+<div style="clear:both;"></div>
+
+
+
+
+
+<div id="wrapper">
+
+<div id="container">
+
+
+
+<div id="main_container">
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--左ナビ-->
+
+
+
+<div id="left_body">
+
+<div id="side_line_down_requ"></div>
+
+<div id="side_block">
+
+
+
+<div class="navi_block">
+
+<div id="home_btn">
+
+<div id="side_totop"><a href="/"><img src="/common/img/navi/side_top_rollout.png" alt="トップページへ" width="220" height="30" border="0"></a></div>
+
+</div>
+
+</div>
+
+
+
+<div id="sidenavi">
+
+
+
+<!--財団概要ブロック-->
+
+<script type="text/javascript" src="/common/js/overview.js"></script>
+
+<script type="text/javascript">
+
+    overview();
+
+</script>
+
+<!--財団概要ブロックここまで-->
+
+
+
+
+
+<!--activityブロック-->
+
+<div class="navi_block">
+
+<div id="navi_b_head"><span>活動内容</span></div>
+
+
+
+
+
+<!--活動内容-->
+
+<ul class="sf-menu sf-vertical">
+
+
+
+
+
+
+
+<!--年間スケジュール-->
+
+<li><a href="/activity/schedule.html"><img src="/common/img/nav_img/nav_activity_sche_rollout.png" alt="年間スケジュール" width="220" height="30" border="0"></a>
+
+</li>
+
+<!--年間スケジュール-->
+
+
+
+
+
+
+
+<!--奨学金支給事業-->
+
+<li><a href="/scholarship/"><img src="/common/img/nav_img/nav_activity_scl_rollout.png" alt="奨学金支給事業" width="220" height="30" border="0"></a>
+
+<ul>
+
+<li><a href="/activity/scholarship_zaidan.html"><img src="/common/img/nav_img/nav_activity_scl_zaidan_rollout.png" alt="一般財団法人 共立国際交流奨学財団奨学金" width="220" height="30" border="0"></a></li>
+
+<li><a href="/activity/scholarship_maintenance.html"><img src="/common/img/nav_img/nav_activity_scl_ment_simple_rollout.png" alt="株式会社共立メンテナンス奨学基金奨学金" width="220" height="30" border="0"></a></li>
+<li><a href="/activity/scholarship_youngsteel.html"><img src="/common/img/nav_img/nav_activity_scl_youngsteel_simple_rollout.png"  alt="ヤングスチール株式会社奨学金" width="220" height="30" border="0"></a></li>
+
+<li><a href="/activity/list.html"><img src="/common/img/nav_img/nav_activity_scl_list_rollout.png" alt="奨学金対象校リスト" width="220" height="30" border="0"></a></li>
+
+
+<li><a href="/information/localscholarship.html" ><img src="/common/img/nav_img/nav_activity_scl_local_rollout.png" alt="現地支給奨学金（ベトナム・ミャンマー・ラオス）" width="220" height="39" border="0"></a></li>
+
+</ul>
+
+</li>
+
+<!--奨学金支給事業-->
+
+
+
+
+
+
+
+<!--生活・学習情報提供事業-->
+
+<li><img src="/common/img/nav_img/nav_life_rollout.png" alt="生活・学習情報提供事業" width="220" height="30" border="0">
+
+<ul>
+
+<li><a href="/activity/publication.html"><img src="/common/img/nav_img/nav_life_pub_rollout.png" alt="出版物の紹介" width="220" height="30" border="0"></a></li>
+
+</ul>
+
+</li>
+
+<!--生活・学習情報提供事業-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--国際交流支援事業-->
+
+<li><img src="/common/img/nav_img/nav_in_rollover.png" alt="国際交流支援事業" width="220" height="30" border="0"></li>
+
+
+<li><a href="/homestay/"><img src="/common/img/nav_img/nav_home_rollover.png" alt="ホームステイ研修会" width="220" height="30" border="0"></a></li>
+
+
+
+
+
+
+<li><a href="/information/kagoshima_kenshuukai.html"><img src="/common/img/nav_img/nav_workshop_rollout.png" alt="財団奨学性研修会" width="220" height="30" border="0"></a></li>
+
+<li><a href="/information/summer_hakodate.html"><img src="/common/img/nav_img/nav_home_hakodate_s_rollout.png" alt="北海道　函館（夏期）" width="220" height="30" border="0"></a></li>
+
+<li><a href="/information/summer_mihoro.html"><img src="/common/img/nav_img/nav_home_mihoro_s_rollout.png" alt="北海道　未幌（夏期）" width="220" height="30" border="0"></a></li>
+
+<li><a href="/information/summer_shibetsu.html"><img src="/common/img/nav_img/nav_home_shibetsu_s_rollout.png" alt="北海道　標津（夏期）" width="220" height="30" border="0"></a></li>
+
+<li><a href="/information/summer_higashison.html"><img src="/common/img/nav_img/nav_home_higashi_s_rollout.png" alt="沖縄　東村（夏期）" width="220" height="30" border="0"></a></li>
+
+<li><a href="/information/winter_shibetsu.html"><img src="/common/img/nav_img/nav_home_shibetsu_w_rollout.png" alt="北海道　標津（冬期）" width="220" height="30" border="0"></a></li>
+
+<li><a href="/information/winter_iejima.html"><img src="/common/img/nav_img/nav_home_iejima_w_rollout.png" alt="沖縄　伊江島（冬期）" width="220" height="30" border="0"><!-- <img src="/common/img/navi/sideinfo.png" alt="募集中" width="51" height="20" border="0" class="bosyu"> --></a></li>
+
+
+
+
+
+
+
+
+
+
+
+<li><a href="/contest_slection/"><img src="/common/img/nav_img/nav_cl_rollout.png" alt="コンテスト・奨学候補生現地選考会" width="220" height="30" border="0"></a></li>
+
+
+<!-- <li><a href="/activity/volunteer.html"><img src="/common/img/nav_img/nav_vol_rollout.png" alt="国際ボランティア支援基金" width="220" height="30" border="0"></a></li> -->
+
+
+
+
+
+
+<!--寮事業-->
+
+<li><a href="/dormy/"><img src="/common/img/nav_img/nav_dormy_rollout.png" alt="寮事業" width="220" height="30" border="0"></a>
+
+<ul>
+
+<li><a href="/placehall/"><img src="/common/img/nav_img/nav_kaikan_rollout.png" alt="国際交流会館" width="220" height="30" border="0"></a></li>
+
+<li><a href="/placehall/03.html"><img src="/common/img/nav_img/nav_kaikan_tsutsuji_rollout.png" alt="つつじヶ丘男子学生会館" width="220" height="30" border="0"></a></li>
+
+<li><a href="/placehall/04.html"><img src="/common/img/nav_img/nav_kaikan_sagami_rollout.png" alt="相模大野学生会館" width="220" height="30" border="0"></a></li>
+
+<li><a href="/dormitory/index.html"><img src="/common/img/nav_img/nav_g_dormy_rollout.png" alt="学習奨励寮" width="220" height="30" border="0"></a></li>
+
+</ul>
+
+</li>
+
+<!--寮事業-->
+
+
+
+</ul>
+
+<!--活動内容-->
+
+
+
+<br style="clear:both">
+
+</div>
+
+<!--activityブロックここまで-->
+
+
+
+
+
+
+
+
+
+<!--officeブロック-->
+
+<script type="text/javascript" src="/common/js/office.js"></script>
+
+<script type="text/javascript">
+
+    office();
+
+</script>
+
+<!--officeブロックここまで-->
+
+
+
+
+
+
+
+
+
+<!--メディア-->
+
+<div class="navi_block">
+
+<ul class="normal_navi n_media">
+
+<li><a href="/media/"><img src="/common/img/nav_img/nav_g_media_rollout.png" alt="メディア掲載" width="220" height="30" border="0"></a></li>
+
+</ul>
+
+</div><!--メディア-->
+
+
+
+
+
+
+
+<!--recruitブロック-->
+
+<script type="text/javascript" src="/common/js/recruit.js"></script>
+
+<script type="text/javascript">
+
+    recruit();
+
+</script>
+
+<!--recruitブロックここまで-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div><!--サイドナビここまで-->
+
+</div><!--side_block-->
+
+
+
+<div style="clear:both;"></div>
+
+
+
+<!--bannerブロック-->
+
+<script type="text/javascript" src="/common/js/banner.js"></script>
+
+<script type="text/javascript">
+
+    banner();
+
+</script>
+
+<!--bannerブロックここまで-->
+
+
+
+</div><!--left_body-->
+
+
+
+<!--左ナビ-->
+
+
+
+
+
+<div id="main">
+
+<div id="main_in">
+
+
+
+
+
+<h1>ホームステイ研修会</h1>
+
+
+
+
+
+
+
+<!--UPDATE 27-->
+
+<div id="asia_map">
+   
+    <img src="/img/homestay/japan_map.png" alt="" width="600" height="671" usemap="#Map" border="0">
+    
+    <map name="Map">
+        <area shape="rect" coords="37,401,150,427" href="/information/kagoshima_kenshuukai.html" class="ise_btn" onClick="">
+        <area shape="rect" coords="64,44,152,71"  href="/information/winter_iejima.html" onClick="" class="iejima_btn">
+        <area shape="rect" coords="179,105,253,132" href="/information/summer_higashison.html" onClick="" class="higashisona_btn">
+        <area shape="rect" coords="415,17,502,45" href="/information/winter_shibetsu.html" onClick="" class="shibetsu_btn">
+        <area shape="rect" coords="323,51,410,79" href="/information/summer_mihoro.html" onClick="" class="mihoro_btn">
+        <area shape="rect" coords="366,166,452,194" href="/information/summer_hakodate.html" onClick="" class="hakodate_btn">
+    </map>
+
+     <div class="hakodate_box">
+        <div id="hakodate_h" class="popup1">
+            <div class="map_center">
+            <!-- <p style="font-weight:bold;font-size:17px; text-align:center; margin:0; ">募集中</p> -->
+            <h2><label style="letter-spacing: -1px;width: 260px;display: inline-block;">2018年8月北海道・函館研修会</label><br>
+            <span>2018年8月24日(金)～8月28日(火)4泊5日</span></h2>
+
+            <a href="/information/summer_hakodate.html"><img src="/img/homestay/h_hakodate.jpg" alt="" width="200" height="134" title=""></a><!--<span style="text-align:center; display:block;">昨年の研修会の様子</span>-->
+            </div>
+            <div style="clear:both;"></div>
+        </div>
+    </div><!--hakodate-->
+
+    <!--通常-->
+    <div class="mihoro_box">
+        <div id="mihoro_h" class="popup4">
+          
+           <!--【HOTESPA】 update 共立財団 20170731-->
+            <div class="map_center" style="width: 280px;margin-left: 25px;">
+                <!-- <p style="font-weight:bold;font-size:17px; text-align:center; margin:0; color: red">募集中</p> -->
+                <h2>2018年8月北海道・美幌研修会<br>
+                <span>2018年8月29日(水)～9月3日(月) 5泊6日</span></h2>
+                 <a href="/information/summer_mihoro.html"><img src="/img/homestay/h_mihoro.jpg" alt="" width="200" height="133" title=""></a>
+                </span>
+                <span style="font-size: 10px;">
+                ※昨年度第105回研修会は応募者が最小催行人数に達しなかったため、不催行となりました。</span>
+            </div>
+            <!--【HOTESPA】 update 共立財団 20170731-->
+            
+            <div style="clear:both;"></div>
+        </div><!--map_center-->
+    </div><!--mihoro-->
+     <!--通常-->
+
+    <!--通常-->
+    <div class="shibetsu_box" style="left:25px !important; ">
+        <div style=" width: 525px;" id="shibetsu_h" class="popup5" >
+            <div class="map_left" style="width: 275px;">
+                <!-- <p style="font-weight:bold;font-size:17px; text-align:center; margin:0; color: red">募集中</p> -->
+                <h2 style="font-size: 130%;">2018年8月北海道・標津研修会<br>
+                <span>2018年8月30日(木)～9月4日(火) 5泊6日</span></h2>
+                <a href="/information/summer_shibetsu.html"><img src="/img/homestay/h_shibetsu_02.jpg" alt="" width="200" height="133" title=""></a>
+            </div>
+            <div class="map_right">
+
+                <h2  style="width: 290px;font-size: 130%;">2019年2月北海道・標津研修会<br>
+                <span>2019年2月14日(木)～2月19日(火)5泊6日</span></h2>
+                <a href="/information/winter_shibetsu.html"><img style="display: block;padding-top: 0; margin-left:26px;text-align: center;" src="/img/homestay/thumb__homestay-shibetsu.jpg" alt="" width="200" height="133" title=""></a>
+            </div>
+            <div style="clear:both;"></div>
+        </div>
+    </div><!--shibetsu-->
+    <!--通常-->
+
+   
+   
+    
+    
+    <!--通常-->
+    <div class="higashisona_box">
+        <div id="higashisona_h" class="popup8" >
+            <div class="map_center" style="width: 260px;margin-left: 33px;">
+                <!-- <p style="font-weight:bold;font-size:17px; text-align:center; margin:0; color: red">募集中</p> -->
+                <h2>2018年9月沖縄・東村研修会<br>
+                <span>2018年9月5日(水)～9月10日(月) 5泊6日</span></h2>
+                <a href="/information/summer_higashison.html"><img src="/img/homestay/h_higashison.jpg" alt="" width="200" height="133" title=""></a>
+            </div>
+            <div style="clear:both;"></div>
+        </div><!--map_center-->
+    </div><!--higashison-->
+    <!--通常-->
+    
+
+    <!--通常-->
+    <div class="iejima_box">
+        <div id="iejima_h" class="popup9">
+            <div class="map_center" style="width: auto;">
+                <h2>2019年2月沖縄・伊江島研修会<br>
+                <span>2019年2月23日(土)～2月28日(木)5泊6日</span></h2>
+                <a href="/information/winter_iejima.html"><img style="display: block; margin-left: 26px;text-align: center;" src="/img/homestay/thumb__homestay-iejima.jpg" alt="" width="200" height="133" title=""></a>
+                <!--<span style="text-align:center; display:block;">昨年の研修会の様子</span>-->
+            </div>
+            <div style="clear:both;"></div>
+        </div>
+    </div><!--iejima-->
+    <!--通常-->
+
+    <div class="ise_box">
+        <div id="ise_h" class="popup10">
+            <div class="map_center">
+                <h2 style="width:300px;margin-left:-15px;font-size: 15px;">第110回奨学生研修会 鹿児島県 鹿児島市<br>
+                <span style="font-size: 11px;">2018年6月8日(金)～6月10日(月) 2泊3日</span></h2>
+                <a href="/information/kagoshima_kenshuukai.html" ><img src="/img/homestay/201806_kagoshima.jpg" alt="" width="200" height="133" title=""></a>
+            </div>
+            <div style="clear:both;"></div>
+        </div>
+    </div><!--higashisona-->
+
+</div><!--asia_map-->
+
+<!--END-->
+
+<div>
+
+
+<!--UPDATE 27 -->
+
+    <div class="con_img left">
+        <h2 style="position: relative;">2019年2月沖縄・伊江島研修会<!-- <a href="/information/winter_iejima.html" style="position: absolute;right: -8px;top: -20px;"><img src="/img/homestay/btn_rec.png" alt="" width="41" height="22" title=""></a> --><br><span>2019年2月23日(土)～2月28日(木)5泊6日</span></h2>
+        <a href="/information/winter_iejima.html"><img src="/img/homestay/thumb__homestay-iejima.jpg" alt="" width="280" height="187" title=""></a>
+    </div><!--con_img-->
+
+    <div class="con_img right">
+        <h2 style="position: relative;">2019年2月北海道・標津研修会<!-- <a href="/information/winter_shibetsu.html" style="position: absolute;right: -17px;top: -20px;"><img src="/img/homestay/btn_rec.png" alt="" width="41" height="22" title=""></a> --><br><span>2019年2月14日(木)～2月19日(火)5泊6日</span></h2>
+        <a href="/information/winter_shibetsu.html"><img src="/img/homestay/thumb__homestay-shibetsu.jpg" alt="" width="280" height="187" title=""></a>
+    </div><!--con_img-->
+
+    <div class="clear"></div>
+
+    <div class="con_img left">
+        <h2 style="position: relative;">2018年9月沖縄・東村研修会<!-- <a href="/information/summer_higashison.html" style="position: absolute;right: -8px;top: -14px;"><img src="/img/homestay/btn_rec.png" alt="" width="41" height="22" title=""></a> --><br><span>2018年9月5日(水)～9月10日(月)5泊6日</span></h2>
+        <a href="/information/summer_higashison.html"><img src="/img/homestay/h_higashison.jpg" alt="" width="280" height="187" title=""></a>
+    </div><!--con_img-->
+    
+    <div class="con_img right">
+        <h2 style="position: relative;">2018年8月北海道・標津研修会<!-- <a href="/information/summer_shibetsu.html" style="position: absolute;right: -17px;top: -14px;"><img src="/img/homestay/btn_rec.png" alt="" width="41" height="22" title=""></a> --><br><span>2018年8月30日(木)～9月4日(火)5泊6日</span></h2>
+        <a href="/information/summer_shibetsu.html"><img src="/img/homestay/h_shibetsu_02.jpg" alt="" width="280" height="187" title=""></a>
+    </div><!--con_img-->
+    
+     <div class="clear"></div>
+     
+     
+     
+     <div class="con_img left">
+        <h2 style="position: relative;">2018年8月北海道・美幌研修会<!-- <a href="/information/summer_mihoro.html" style="position: absolute;right: -17px;top: -14px;"><img src="/img/homestay/btn_rec.png" alt="" width="41" height="22" title=""></a> --><br><span>2018年8月29日(水)～9月3日(月)5泊6日</span></h2>
+        <a href="/information/summer_mihoro.html"><img src="/img/homestay/h_mihoro.jpg" alt="" width="280" height="187" title=""></a>
+    </div><!--con_img-->
+    
+      <div class="con_img right">
+        <h2 style="position: relative;">2018年8月北海道・函館研修会<!-- <a href="/information/summer_higashison.html" style="position: absolute;right: -8px;top: -14px;"><img src="/img/homestay/btn_rec.png" alt="" width="41" height="22" title=""></a> --><br><span>2018年8月24日(金)～8月28日(火)4泊5日</span></h2>
+        <a href="/information/summer_hakodate.html"><img src="/img/homestay/h_hakodate.jpg" alt="" width="280" height="187" title=""></a>
+    </div><!--con_img-->
+    
+     <div class="clear"></div>
+     
+     
+    
+    <div class="con_img left">
+        <h2><label for="">第110回研修会 鹿児島県 鹿児島市</label><br><span>2018年6月8日(金)～6月10日(月) 2泊3日</span></h2>
+        <a href="/information/kagoshima_kenshuukai.html"><img src="/img/homestay/201806_kagoshima.jpg" alt="" width="280" height="187" title=""></a>
+    </div>
+    <!--con_img-->
+     <div class="clear"></div>
+     
+  
+
+<!--END-->
+
+</div>
+
+
+
+
+
+<div class="clear"></div>
+
+
+
+
+
+
+
+
+
+<!--<div style="  margin-top: 50px;
+
+  margin-left: 10px;"><a href="/information/2014home.html"><img src="/information/img/2014_home_rollout.png" alt="" width="580" height="30"></a></div>-->
+
+
+
+
+
+<div style="clear:both; height:0;"></div>
+
+
+<!-- 20170316 追加 -->
+<div class="library">
+<div class="inner">
+<p>各研修会の動画はこちら</p>
+<a href="/movie/"><img src="/img/movie_library_rollout.gif" alt="動画ライブラリー"></a>
+</div>
+</div>
+<!-- /20170316 追加 -->
+
+
+</div><!--main_in-->
+
+</div><!--main-->
+
+</div><!--main container-->
+
+</div><!--end container-->
+
+<div style="clear:both;"></div>
+
+
+
+<div id="back-top_pos_wide">
+
+<p id="back-top_wide"><a href="#top"><span><img src="/img/to_top.png" alt="ページトップへ戻る" width="30" height="130"></span></a></p>
+
+</div>
+
+
+
+
+
+</div><!--end wrapper-->
+
+
+
+<!--foot-->
+
+<script type="text/javascript" src="/common/js/foot.js"></script>
+
+<script type="text/javascript">
+
+    foot();
+
+</script>
+
+<!--foot-->
+
+
+
+</div><!--end back1-->
+
+</body>
+</html>
